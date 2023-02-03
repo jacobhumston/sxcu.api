07e74ca08e3ffe768df2cb673f81ff809e0fc79a
jacobhumston-school <jhumston24@sciototech.org>
2023-02-03 10:55:10 -0500

    snowflake testing- not finished nor working

--- test/index.js
+++ test/index.js
@@ -2,25 +2,13 @@
 const { files, subdomains, collections, text, links, utility } = require('../');
 const secrets = require('./secrets.json');
 
-async function test() {
-    const collection = await collections.createCollection('test', 'lol idk', true, false).catch((e) => {
-        throw e;
-    });
-    console.log(collection);
-    while (true) {
-        await files
-            .uploadFile('test/a-test-min.png', {
-                collection: collection.collectionId,
-                collectionToken: collection.collectionToken,
-            })
-            .then(function (data) {
-                console.log('Success!', data.url);
-            })
-            .catch(function (err) {
-                console.log('Error!', utility.resolveError(err));
-            });
-        await utility.getRateLimitPromise('uploadFile');
-    }
-}
+async function test() {}
 
 test();
+
+function convertSnowflake(snowflake) {
+    const buffer = Buffer.from(snowflake);
+    return parseInt(buffer.toString(), 10)
+}
+
+console.log(convertSnowflake('539SYFuIC'));
