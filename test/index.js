a299f30337008422066bf576ab88c0a183833585
jacobhumston-school <jhumston24@sciototech.org>
2023-02-07 09:22:37 -0500

    removed snowflake testing

--- test/index.js
+++ test/index.js
@@ -5,17 +5,3 @@ const secrets = require('./secrets.json');
 async function test() {}
 
 test();
-
-function convertSnowflake(snowflake) {
-    function encode(string) {
-        var number = "0x";
-        var length = string.length;
-        for (var i = 0; i < length; i++)
-            number += string.charCodeAt(i).toString(10);
-        return number;
-    }
-    
-    console.log(encode(snowflake))
-}
-
-console.log(convertSnowflake('539SYFuIC'));
