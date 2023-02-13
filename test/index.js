a22b54bb4af926732c05509acfac0756a211efd2
jacobhumston-school <jhumston24@sciototech.org>
2023-02-13 08:58:53 -0500

    .

--- test/index.js
+++ test/index.js
@@ -4,26 +4,13 @@ const secrets = require('./secrets.json');
 
 async function test() {
     const queue = new utility.queue();
-    const images = ['test!!', 'test!!', 'test!!'];
-    images.forEach(function (image, index) {
-        const progress = ((index + 1) / images.length) * 100;
-        queue
-            .push(function () {
-                console.log('UPLOADING...');
-                return text.createPaste(`test/${image}`);
-            }, 'createPaste')
-            .then(function (data) {
-                if (data.error) {
-                    console.log(Math.round(progress) + '%', 'ERROR!', data.error);
-                } else {
-                    console.log(Math.round(progress) + '%', 'UPLOADED!', data.url);
-                }
-                if (progress === 100) {
-                    console.log('DONE!');
-                    queue.stop();
-                }
-            });
-    });
+    queue
+        .push(() => {
+            console.log('This method is executing.');
+        })
+        .then(() => {
+            console.log('This method is finished executing.');
+        });
 }
 
 test();
