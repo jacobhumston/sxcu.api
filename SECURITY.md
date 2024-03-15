c77ba770d647bc50a2ce14a74a97c0c51be82ebb
Jacob Humston's School Account <113031145+jacobhumston-school@users.noreply.github.com>
2024-03-15 13:10:09 +0000

    Uploaded banner.png.

--- SECURITY.md
+++
@@ -1,5 +0,0 @@
-# Security Policy
-
-## Reporting a Vulnerability
-
-Create an issue https://github.com/Lovely-Experiences/sxcu.api/issues.
--- test/index.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable */
-const { files, subdomains, collections, text, links, utility } = require('../');
-
-const converted = utility.convertSxcuFile('test/example-3.sxcu');
-files.uploadFile('test/test.png', converted.options, converted.domain).then(function (response) {
-    console.log(response.url);
-});
