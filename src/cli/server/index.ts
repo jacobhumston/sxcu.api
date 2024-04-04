3d5b646b4d6213e3a91a6cb69cefae53c03b4837
Jacob Humston's School Account <113031145+jacobhumston-school@users.noreply.github.com>
2024-04-04 15:40:40 +0000

    .

--- src/cli/server/index.ts
+++ src/cli/server/index.ts
@@ -1,5 +1,9 @@
 import { ParsedOption } from '../createCommand.js';
+import { logo } from './text-logo.js';
 
 export async function main(options: ParsedOption[]) {
-    console.log(options);
+    console.clear();
+    const port = options[0].value;
+    console.log(logo);
+    console.log(`Running on port ${port}...`);
 }
