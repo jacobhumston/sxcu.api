3a0bedf24afb358727f2beab22957b13660574f0
Jacob Humston <jhumston24@sciototech.org>
2024-04-16 12:04:40 -0400

    Updated docs branch.

--- src/cli/commands/userAgent.ts
+++
@@ -1,7 +0,0 @@
-import createCommand from '../createCommand.js';
-import { UserAgent } from 'sxcu.api';
-import { logger } from '../logger.js';
-
-export default createCommand('user-agent', 'Log the current userAgent used for web requests.', [], async function () {
-    logger.info(UserAgent.get());
-});
--- test/tests/uploadFile-convertSxcuFile.js
+++
@@ -1,9 +0,0 @@
-import { convertSxcuFile, uploadFile } from 'sxcu.api';
-
-export const active = false;
-
-export async function execute() {
-    const sxcuFile = convertSxcuFile('test/assets/test.sxcu');
-    const uploadedFile = await uploadFile('test/assets/test.png', sxcuFile.options, sxcuFile.domain);
-    console.log(uploadedFile);
-}
